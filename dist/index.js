29adc5b10bb4e1826e8a38dc7234104ab950a4de
Cecilia Giusti <ceciliagiusti2b@gmail.com>
2022-10-27 11:45:56 +0200

    Fix new babel

--- dist/index.js
+++ dist/index.js
@@ -1,25 +1,17 @@
-"use strict";
-
 Object.defineProperty(exports, "__esModule", {
-  value: true
+  value: true,
 });
-exports.DropdownMenu = void 0;
-require("core-js/modules/es.array.sort.js");
-var _react = _interopRequireDefault(require("react"));
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 /**
  * Module
  * @function
  * @param {Array} options - options of dropdown
  * @returns {HTMLOptionElement}
  */
-const DropdownMenu = _ref => {
-  let {
-    options
-  } = _ref;
+const DropdownMenu = (_ref) => {
+  let { options } = _ref;
   if (Array.isArray(options) && typeof options[0] === "string") {
     const optionsSort = options.sort();
-    return optionsSort.map(option => {
+    return optionsSort.map((option) => {
       return <option key={option}>{option}</option>;
     });
   } else {
@@ -32,9 +24,10 @@ const DropdownMenu = _ref => {
       }
       return 0;
     });
-    return optionsSort.map(option => {
+    return optionsSort.map((option) => {
       return <option key={option.name}>{option.name}</option>;
     });
   }
 };
-exports.DropdownMenu = DropdownMenu;
+const _DropdownMenu = DropdownMenu;
+export { _DropdownMenu as DropdownMenu };
